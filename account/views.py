2c5073d73f4c73ad8f0be437239aeeafe1510362
sirdesmond09 <sirdesmond09@gmail.com>
2021-12-11 10:32:08 +0100

    added profile picture to login data

--- account/views.py
+++ account/views.py
@@ -314,6 +314,7 @@ def user_login(request):
                         user_detail['phone'] = user.phone
                         user_detail['is_admin'] = user.is_admin
                         user_detail['is_staff'] = user.is_staff
+                        user_detail['profile_pics_url'] = user.profile_pics_url
                         user_detail['access'] = str(refresh.access_token)
                         user_detail['refresh'] = str(refresh)
                         user_logged_in.send(sender=user.__class__,
