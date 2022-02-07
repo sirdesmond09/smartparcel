9fea488320c3d153c7b057c283a8906591f15bff
sirdesmond09 <sirdesmond09@gmail.com>
2022-02-07 19:55:54 +0100

    add static files

--- config/settings.py
+++ config/settings.py
@@ -141,7 +141,7 @@ class Common(Configuration):
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/3.0/howto/static-files/
     STATIC_URL = '/static/'
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
     # Default primary key field type
@@ -153,7 +153,7 @@ class Common(Configuration):
     AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'account.authentication.UserAuthBackend'
-]
+    ]
 
 
     SOCIAL_AUTH_FACEBOOK_KEY = os.getenv('SOCIAL_AUTH_FACEBOOK_KEY')
